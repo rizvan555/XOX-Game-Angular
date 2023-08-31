df6ff87e68cd034e69929edbfa077d71c471b22d
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-31 22:29:30 +0200

    created new XOX game and new game button with resresh old game to new game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'game-angular';
+  box: string = '';
+  message: string = '';
+  games: string[] = [];
+
+  clickBox(index: number) {
+    this.games[index] = this.box;
+    if (this.box == 'X') this.box = 'O';
+    else this.box = 'X';
+    this.message = `Your next step: ${this.box}`;
+  }
+  newGame() {
+    this.box = 'X';
+    this.message = 'Next-X';
+    this.games = ['', '', '', '', '', '', '', '', ''];
+  }
 }
